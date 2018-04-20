4cb156c20be6e3f4b2881403643cb830e87756d4
loveacat <yahoo.007@163.com>
2018-04-20 10:42:34 +0800

    add area numbers and remove function

--- src/pages/FleetArea.js
+++ src/pages/FleetArea.js
@@ -8,7 +8,8 @@ class FleetArea extends Component {
       const self =this;
       this.state = {
         position:{ lng: 116.402544, lat: 39.928216 },
-        what:"点击按钮开始绘制多边形"
+        what:"点击add按钮开始绘制",
+        numbers: 0,
       };
       this.mapEvents ={
         created: (mapinst) => {
@@ -22,7 +23,14 @@ class FleetArea extends Component {
           self.tool = tool;
         },
         draw({obj}) {
+            if (self.tool){
+              self.tool.close();
+            }
+            self.setState({
+              what: '关闭了绘图工具'
+            });
             console.log("new obj",self.mapinst.getAllOverlays())
+            self.setState({numbers:self.mapinst.getAllOverlays().length})
             // self.mapinst.plugin(["AMap.PolyEditor"],function(){
             //     let polylineEditor = new self.mapinst.PolyEditor(self.mapinst,obj); 
             //     polylineEditor.open(); 
@@ -34,7 +42,7 @@ class FleetArea extends Component {
         if (this.tool) {
           this.tool.polygon();
           this.setState({
-            what: '准备绘制多边形'
+            what: '准备绘制，双击鼠标结束'
           });
         }
       }
@@ -46,6 +54,12 @@ class FleetArea extends Component {
           what: '关闭了鼠标工具'
         });
       }
+    clear =()=>{
+      if(this.mapinst){
+        this.mapinst.clearMap();
+      }
+      this.setState({numbers:0})
+    }
     render() {
       return (
           <div className="App"> 
@@ -57,8 +71,9 @@ class FleetArea extends Component {
             >
                <MouseTool events={this.toolEvents}/>
                <div className="layerStyle">{this.state.what}</div>
-               <button className="button1Style" onClick={()=>{this.drawPolygon()}}>add Area</button>
-               <button className="button2Style" onClick={()=>{this.close()}}>Close</button>
+               <button className="button1Style" onClick={()=>{this.drawPolygon()}}><p className="buttontext">Add Area</p></button>
+               <button className="button2Style"><p className="buttontext">Areas : {this.state.numbers}</p></button>
+               <button className="button3Style" onClick={this.clear}><p className="buttontext">remove all Areas</p></button>
             </Map>
 
           </div>
